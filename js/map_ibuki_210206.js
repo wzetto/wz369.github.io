ed37e9bc798407e228ef59f85ad152d2054e05ce
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-01 01:58:04 +0900

    Update map_ibuki_210206.js

--- js/map_ibuki_210206.js
+++ js/map_ibuki_210206.js
@@ -6,4 +6,6 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var gpx = 'https://wzetto.github.io/wz369.github.io/yamanobo/ibuki/210206_ibuki.gpx';
 new L.GPX(gpx, {async: true}).on('loaded', function(e) {
   map.fitBounds(e.target.getBounds());
+  map.fitBounds(e.target.get_distance());
+  map.fitBounds(e.target.get_elevation_gain());
 }).addTo(map);
