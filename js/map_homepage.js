84c388bd3b1b474fad6fcd3527793663a0274490
wzetto <85636194+wzetto@users.noreply.github.com>
2021-12-31 23:05:06 +0900

    Update map_homepage.js

--- js/map_homepage.js
+++ js/map_homepage.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([35.023151, 135.804174], 15);
+var map = L.map('map').setView([35.023151, 135.804174], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid3pldHRvIiwiYSI6ImNreHVnMTRuODVzdW4yeXFxYTgxM3dyanUifQ.lOyz4mpFDxjWo7qWeq6AYA', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
