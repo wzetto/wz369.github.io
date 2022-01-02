dd7d34cc6632262da112d1f5d2e2d73c1b6a88d6
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-02 19:21:58 +0900

    Update header_scroll.js

--- js/header_scroll.js
+++ js/header_scroll.js
@@ -2,7 +2,7 @@ window.inscroll = function()
 {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100/*|| document.documentElement.scrollTop > 100*/) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementsByClassName("inner_top background_img")[0].style.height = "100px";
     document.getElementsByClassName("inner_top background_img")[0].style.backgroundSize = "cover";
     document.getElementById("project_title").style.fontSize = "26px";
