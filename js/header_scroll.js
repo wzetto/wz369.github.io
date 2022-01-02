d81790eb5bbb6638ffce40b572196009512304b8
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-02 20:14:40 +0900

    Update header_scroll.js

--- js/header_scroll.js
+++ js/header_scroll.js
@@ -5,8 +5,7 @@ window.onscroll = function() {
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementsByClassName("inner_top background_img")[0].style.height = "40px";
-    document.getElementById("project_title").style.position = "absolute";
-    document.getElementById("project_title").style.top = "10px";
+    document.getElementById("project_title").style.top = "0%";
   } else {
     document.getElementsByClassName("inner_top background_img")[0].style.height = "300px";
     document.getElementById("project_title").style.position = "relative";
