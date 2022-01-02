cfb718b1ffc592599470306f840b1b3d587c6cfe
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-02 18:50:03 +0900

    Create header_scroll.js

---
+++ js/header_scroll.js
@@ -0,0 +1,12 @@
+window.inscroll = function()
+{scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementsByClassName("inner_top background_img")[0].style.height = "100px";
+    document.getElementsByClassName("inner_top background_img")[0].style.backgroundSize = "cover";
+    document.getElementById("project_title").style.fontSize = "26px";
+    document.getElementById("project_title").style.fontSize = "26px";
+  } else {/*may stay the origin*/
+  }
+}
