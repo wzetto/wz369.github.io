afaf31440564c3ae8d9dac07b7e5e3156aeb08b9
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-03 19:48:52 +0900

    Update bottom_text.js

--- js/bottom/bottom_text.js
+++ js/bottom/bottom_text.js
@@ -5,11 +5,11 @@ $(window).scroll(function() {
     var top_of_screen = $(window).scrollTop();
 
     if (((bottom_of_screen < top_of_element) || (top_of_screen > bottom_of_element)) 
-        && ($("#bottom_text").text().indexOf('天国から地獄') != -1)) {
-        document.getElementById("bottom_text").innerHTML = '地獄から天国';
+        && ($("#bottom_text").text().indexOf('天国から地獄.') != -1)) {
+        document.getElementById("bottom_text").innerHTML = '地獄から天国.';
         //$("#bottom_text").text('abc');
     } else if (((bottom_of_screen < top_of_element) || (top_of_screen > bottom_of_element)) 
-        && ($("#bottom_text").text().indexOf('地獄から天国') != -1)) {
+        && ($("#bottom_text").text().indexOf('地獄から天国.') != -1)) {
         //document.getElementsByClassName("div.bottom_text")[0].innerHTML = '天国から地獄';
         $("#bottom_text").text('cba');
     } else {
