15c06a350886f4fad19f77ae0627087ee487ea7e
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-03 18:51:28 +0900

    Update bottom_text.js

--- js/bottom/bottom_text.js
+++ js/bottom/bottom_text.js
@@ -1,16 +1,16 @@
 $(window).scroll(function() {
-    var top_of_element = $("div.bottom_text").offset().top;
-    var bottom_of_element = $("div.bottom_text").offset().top + $("div.bottom_text").outerHeight();
+    var top_of_element = $(".bottom_text").offset().top;
+    var bottom_of_element = $(".bottom_text").offset().top + $(".bottom_text").outerHeight();
     var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
     var top_of_screen = $(window).scrollTop();
 
     if (((bottom_of_screen < top_of_element) || (top_of_screen > bottom_of_element)) 
-        && $("div.bottom_text").text() == '天国から地獄') {
+        && $(".bottom_text").text() == '天国から地獄') {
         //document.getElementsByClassName("div.bottom_text")[0].innerHTML = '地獄から天国';
-        $("div.bottom_text").text('abc')
+        $(".bottom_text").text('abc')
     } else if (((bottom_of_screen < top_of_element) || (top_of_screen > bottom_of_element)) 
-        && $("div.bottom_text").text() == '地獄から天国') {
+        && $(".bottom_text").text() == '地獄から天国') {
         //document.getElementsByClassName("div.bottom_text")[0].innerHTML = '天国から地獄';
-        $("div.bottom_text").text('天国から地獄')
+        $(".bottom_text").text('天国から地獄')
     }
 });
