5b04c3efa061bd3e71fd004a9d85ac50a9025857
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-04 20:42:13 +0900

    Update header_scroll.js

--- js/header/header_scroll.js
+++ js/header/header_scroll.js
@@ -1,13 +1,20 @@
-window.onscroll = function() {
-  scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-    document.getElementsByClassName("inner_top background_img")[0].style.height = "40px";
+var beforeScrollTop = $(window).scrollTop();
+$(window).on("scroll", function (event) {
+  var scrollTop = $(window).scrollTop();
+  var delta = false;
+  // after scrolling
+  var afterScrollTop = $(window).scrollTop();
+  var delta = afterScrollTop - beforeScrollTop;
+  
+  if (delta > 5) {
+    document.getElementsByClassName("inner_top background_img1")[0].style.height = "40px";
     document.getElementById("project_title").style.top = "-20%";
-  } else {
-    document.getElementsByClassName("inner_top background_img")[0].style.height = "300px";
+  } else if (delta < -30) {
+    document.getElementsByClassName("inner_top background_img1")[0].style.height = "360px";
+    document.getElementById("project_title").style.top = "5%";
+  } else if (scrollTop <= 1) {
+    document.getElementsByClassName("inner_top background_img1")[0].style.height = "360px";
     document.getElementById("project_title").style.top = "5%";
   }
-}
+  beforeScrollTop = afterScrollTop;
+});
