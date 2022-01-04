1134147f06944724d8480cea26c2e0e2b3ccddf7
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-04 22:09:59 +0900

    Update header_scroll2.js

--- js/header/header_scroll2.js
+++ js/header/header_scroll2.js
@@ -10,14 +10,17 @@ $(window).on("scroll", function (event) {
     document.getElementsByClassName("inner_top background_img2")[0].style.height = "40px";
     document.getElementById("project_title").style.top = "-20%";
     document.getElementsByClassName("home_bar")[0].style.display = "none";
+    document.getElementsByClassName("home_bar")[1].style.display = "none";
   } else if (delta < -30) {
     document.getElementsByClassName("inner_top background_img2")[0].style.height = "360px";
     document.getElementById("project_title").style.top = "5%";
     document.getElementsByClassName("home_bar")[0].style.display = "block";
+    document.getElementsByClassName("home_bar")[1].style.display = "block";
   } else if (scrollTop <= 1) {
     document.getElementsByClassName("inner_top background_img2")[0].style.height = "360px";
     document.getElementById("project_title").style.top = "5%";
     document.getElementsByClassName("home_bar")[0].style.display = "block";
+    document.getElementsByClassName("home_bar")[1].style.display = "block";
   }
   beforeScrollTop = afterScrollTop;
 });
