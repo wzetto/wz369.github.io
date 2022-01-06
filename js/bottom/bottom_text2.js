ad6dc95a58c6c036afe65b15428ba11bc30e20cd
wzetto <85636194+wzetto@users.noreply.github.com>
2022-01-06 11:58:17 +0900

    Update bottom_text2.js

--- js/bottom/bottom_text2.js
+++ js/bottom/bottom_text2.js
@@ -5,12 +5,12 @@ $(window).scroll(function() {
     var top_of_screen = $(window).scrollTop();
 
     if ((bottom_of_screen < top_of_element) || (top_of_screen > bottom_of_element)) {
-        if (document.getElementById("bottom_text").innerHTML == '有り得ざる妖精の王よ——') {
+        if (document.getElementById("bottom_text").innerHTML == '有り得ざる妖精の王よ') {
                 document.getElementById("bottom_text").innerHTML = 'Life is shit but I\'m loving it.';
         //$("#bottom_text").text('abc');
     } else if (document.getElementById("bottom_text").innerHTML == 'Life is shit but I\'m loving it.') {
         //document.getElementsByClassName("div.bottom_text")[0].innerHTML = '天国から地獄';
-        document.getElementById("bottom_text").innerHTML = '有り得ざる妖精の王よ——';
+        document.getElementById("bottom_text").innerHTML = '有り得ざる妖精の王よ';
     } else {
         document.getElementById("bottom_text").innerHTML = 'I dont understand';
     }}
