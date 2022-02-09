22317642580a8c848a2926d50766345eef4b5c7c
wzetto <85636194+wzetto@users.noreply.github.com>
2022-02-09 18:05:03 +0900

    Update header_scroll_formal.js

--- js/header/header_scroll_formal.js
+++ js/header/header_scroll_formal.js
@@ -15,14 +15,16 @@ $(window).on("scroll", function (event) {
 	    barElem[i].style.display = "none";
     }
   } else if (delta < -30) {
-    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = "360px";
+    document.getElementsByClassName("inner_top background_img_formal")[0].style.minHeight = "150px";
+    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = "auto";
     document.getElementById("project_title").style.top = "5%";
     
     for (var i = 0; i < barElem.length; i++){
 	    barElem[i].style.display = "block";
     }
   } else if (scrollTop <= 1) {
-    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = "360px";
+    document.getElementsByClassName("inner_top background_img_formal")[0].style.minHeight = "150px";
+    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = "auto";
     document.getElementById("project_title").style.top = "5%";
     
     for (var i = 0; i < barElem.length; i++){
