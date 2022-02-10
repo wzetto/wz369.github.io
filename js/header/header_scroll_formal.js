2e6a019967dc707e5257c8bda549433cc0e366c9
wzetto <85636194+wzetto@users.noreply.github.com>
2022-02-10 15:12:59 +0900

    Update header_scroll_formal.js

--- js/header/header_scroll_formal.js
+++ js/header/header_scroll_formal.js
@@ -8,11 +8,10 @@ $(window).on("scroll", function (event) {
   var barElem = document.getElementsByClassName("home_bar");
   var width = $(window).width();
   if (width <= 992) {
-	  var headerHeight = 160;
+	  var headerHeight = "160px";
   } else if (width > 992) {
-	  let headerHeight = 300;
+	  let headerHeight = "300px";
   }
-  var heightSettle = `${headerHeight}` + "px";
   
   if (delta > 5) {
     document.getElementsByClassName("inner_top background_img_formal")[0].style.height = "40px";
@@ -22,14 +21,14 @@ $(window).on("scroll", function (event) {
 	    barElem[i].style.display = "none";
     }
   } else if (delta < -30) {
-    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = heightSettle;
+    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = headerHeight;
     document.getElementById("project_title").style.top = "5%";
     
     for (var i = 0; i < barElem.length; i++){
 	    barElem[i].style.display = "block";
     }
   } else if (scrollTop <= 1) {
-    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = heightSettle;
+    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = headerHeight;
     document.getElementById("project_title").style.top = "5%";
     
     for (var i = 0; i < barElem.length; i++){
