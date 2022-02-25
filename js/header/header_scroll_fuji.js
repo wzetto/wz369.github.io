619311ee9c7cfa93a6e741a481d64fa77268b036
wzetto <85636194+wzetto@users.noreply.github.com>
2022-02-25 14:41:22 +0900

    Update header_scroll_fuji.js

--- js/header/header_scroll_fuji.js
+++ js/header/header_scroll_fuji.js
@@ -21,14 +21,14 @@ $(window).on("scroll", function (event) {
 	    barElem[i].style.display = "none";
     }
   } else if (delta < -30) {
-    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = headerHeight;
+    document.getElementsByClassName("inner_top background_img_fuji")[0].style.height = headerHeight;
     document.getElementById("project_title").style.top = "5%";
     
     for (var i = 0; i < barElem.length; i++){
 	    barElem[i].style.display = "block";
     }
   } else if (scrollTop <= 1) {
-    document.getElementsByClassName("inner_top background_img_formal")[0].style.height = headerHeight;
+    document.getElementsByClassName("inner_top background_img_fuji")[0].style.height = headerHeight;
     document.getElementById("project_title").style.top = "5%";
     
     for (var i = 0; i < barElem.length; i++){
